d1103387b582d297edd8141a91a5a21aa82f6b8a
Aniruddha Sen <87408590+Anisen123@users.noreply.github.com>
2023-11-27 20:28:21 -0500

    updated oh_scheduler.py

--- oh_scheduler.py
+++ oh_scheduler.py
@@ -1,5 +1,6 @@
 from collections import Counter
 import random
+import pandas as pd
     
 def find_common_times(availability, rankings, n, m):
     all_times = []
@@ -21,45 +22,51 @@ def find_common_times(availability, rankings, n, m):
                 common_times_rankings[time] += (3 - student_rankings.index(time))
     sorted_common_times = sorted(common_times, key=lambda x: common_times_rankings[x], reverse=True)
     
-    return sorted_common_times[:5]
+    return sorted_common_times[:n]
 
-# test data
-availability = []
-rankings = []
-for i in range(20):
-    # 6 random available times for each student in a week
-    available_times = random.sample(range(1, 25), 6)
-    availability.append(available_times)
-    
-    # top 3 ranked times for each student
-    ranked_times = random.sample(available_times, 3)
-    rankings.append(ranked_times)
+def map_times_to_array(file_path,bool):
+    df = pd.read_csv(file_path)
+    df = df.drop(df.columns[0], axis=1)
+    column_names = df.columns.tolist()
+    if bool:
+        for index, row in df.iterrows():
+            for column in df.columns:
+                new_value = f"{column} {row[column]}"
+                df.at[index, column] = new_value
 
-def map_times_to_week(availability):
-    # create a dictionary to map each available time to a day/time in the week
-    time_map = {}
-    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    times = [ '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM']
-    for i, time in enumerate(sorted(set([item for sublist in availability for item in sublist]))):
-        day = days[i % 7]
-        time_map[time] = f"{day} {times[(time-1)%12]}"
-    return time_map
+    print(df)
 
-# test function
-n = 5
-m = 5
+    # Create a dictionary to hash each element to a unique integer
+    element_map = {}
+    for column in column_names:
+        for cell in df[column]:
+            if cell not in element_map:
+                element_map[cell] = hash(cell)
+
+    # Create an array of arrays with each subarray representing a row of the new hashed availibility list
+    new_avlist = []
+    for i in range(len(df)):
+        row_array = []
+        for column in column_names:
+            cell = df[column][i]
+            row_array.append(element_map[cell])
+        new_avlist.append(row_array)
+
+    return element_map, new_avlist
+
+
+# test function to return n most common times with at least m students attending, inputing data from csv files
+map1, availability = map_times_to_array("Student_Availability.csv",True)
+map2, rankings = map_times_to_array("Student_Rankings.csv",False)
+#print(availability)
+#print(rankings)
+
+n = 2
+m = 1
 common_times = find_common_times(availability, rankings, n, m)
-time_map = map_times_to_week(availability)
-print("Availability:")
-for i, student_time in enumerate(availability):
-    print(f"Student {i+1}:")
-    for time in student_time:
-        print(f"\t{time_map[time]}")
-print("Rankings:")
-for i, student_rankings in enumerate(rankings):
-    print(f"Student {i+1}:")
-    for j, time in enumerate(student_rankings):
-        print(f"\t{j+1}. {time_map[time]}")
-print("Most Popular Times:")
-for time in common_times:
-    print(f"\t{time_map[time]}")
+
+# Run inverse of hash function to get the original time strings
+inverse_hash_times = {v: k for k, v in map1.items()}
+common_times_original = [inverse_hash_times[time] for time in common_times]
+
+print(common_times_original)
