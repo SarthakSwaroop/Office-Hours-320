05b6f4c7488243c3d621927478517ae7ee2596f2
Sarthak Swaroop Bansal <52138161+SarthakSwaroop@users.noreply.github.com>
2023-12-05 03:17:07 +0530

    Update OfficeHoursList.js

--- FrontEnd/HomePage/src/components/OfficeHoursList/OfficeHoursList.js
+++ FrontEnd/HomePage/src/components/OfficeHoursList/OfficeHoursList.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './OfficeHoursList.css';
 import CalendarIcon from '../../assets/icons/calender1.svg';
 
 function OfficeHoursList() {
-  const officeHours = [
-    { id: 1, date: 'Monday, October 23, 2023', time: '15:00', professor: 'Professor Mathew Rattigan', course: 'CS 320' },
-    { id: 2, date: 'Monday, October 23, 2023', time: '15:00', professor: 'Professor Mathew Rattigan', course: 'CS 320' },
-    { id: 3, date: 'Monday, October 23, 2023', time: '15:00', professor: 'Professor Mathew Rattigan', course: 'CS 320' },
-  ];
+  const [officeHours, setOfficeHours] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); 
+
+  useEffect(() => {
+    const fetchOfficeHours = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/office-hours');
+        setOfficeHours(response.data);
+      } catch (error) {
+        console.error('Error fetching office hours:', error);
+        // Handle error here (e.g., show user feedback)
+      } finally {
+        setIsLoading(false); // Set loading to false after fetching data
+      }
+    };
+
+    fetchOfficeHours();
+  }, []);
 
   return (
     <div className="office-hours-list">
       <h2>Upcoming Office Hours</h2>
-      {officeHours.map((session) => (
-        <div key={session.id} className="office-hour-entry">
-          <div className="office-hour-date">{session.date}</div>
-          <div className="office-hour-details">
-            <div className="office-hour-time">
-              <span>{session.time}</span>
-              <img src={CalendarIcon} alt="Calendar" className="calendar-icon" />
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : officeHours.length > 0 ? (
+        officeHours.map((session) => (
+          <div key={session.id} className="office-hour-entry">
+            <div className="office-hour-date">{session.date}</div>
+            <div className="office-hour-details">
+              <div className="office-hour-time">
+                <span>{session.time}</span>
+                <img src={CalendarIcon} alt="Calendar" className="calendar-icon" />
+              </div>
+              <span className="office-hour-professor">{session.professor}</span>
+              <span className="office-hour-course">{session.course}</span>
+              <button className="modify-button">Modify</button>
             </div>
-            <span className="office-hour-professor">{session.professor}</span>
-            <span className="office-hour-course">{session.course}</span>
-            <button className="modify-button">Modify</button>
           </div>
-        </div>     
-      ))}
+        ))
+      ) : (
+        <div>No upcoming office hours</div>
+      )}
     </div>
   );
 }
