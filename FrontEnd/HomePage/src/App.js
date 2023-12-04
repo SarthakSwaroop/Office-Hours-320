c0ebf1603574b4ba0ecedc0ac98b4d0e0a40b2ae
Sarthak Swaroop Bansal <52138161+SarthakSwaroop@users.noreply.github.com>
2023-12-05 03:14:51 +0530

    Update App.js

--- FrontEnd/HomePage/src/App.js
+++ FrontEnd/HomePage/src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import OfficeHoursList from './components/OfficeHoursList/OfficeHoursList';
 import SuggestedTimings from './components/SuggestedTimings/SuggestedTimings';
+import StudentOfficeHoursList from './components/StudentOfficeHoursList/StudentOfficeHoursList';
 import TopBar from './components/TopBar/TopBar';
 import './App.css';
+import CalendarPage from './components/CalendarPage/CalendarPage';
+const ProfilePage = () => <div>Profile Page Content</div>;
 
 function App() {
+  const [isStudent, setIsStudent] = useState(false); 
+
   return (
-    <div className="app">
-      <Header />
-      <div className="main-content">
-        <Sidebar />
-        <div className="content-wrapper">
-          <TopBar />
-          <OfficeHoursList />
-          <SuggestedTimings />
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="main-content">
+          <Sidebar />
+          <div className="content-wrapper">
+            <Routes>
+              <Route path="/" element={isStudent ? <><TopBar /><StudentOfficeHoursList /></> : <><TopBar /><OfficeHoursList /><SuggestedTimings /></>} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/calendar" element={<CalendarPage />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
