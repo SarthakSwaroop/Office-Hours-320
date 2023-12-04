9919280bd30ae46dc520e9aa03be3c2e89afde65
Sarthak Swaroop Bansal <52138161+SarthakSwaroop@users.noreply.github.com>
2023-12-05 03:16:40 +0530

    Update Sidebar.js

--- FrontEnd/HomePage/src/components/Sidebar/Sidebar.js
+++ FrontEnd/HomePage/src/components/Sidebar/Sidebar.js
@@ -4,18 +4,22 @@ import HomeIcon from '../../assets/icons/home.svg';
 import ProfileIcon from '../../assets/icons/profile.svg';
 import NotificationIcon from '../../assets/icons/notification.svg';
 import CalenderIcon from '../../assets/icons/calender.svg';
+import { Link } from 'react-router-dom';
 
 function Sidebar() {
-  console.log('Sidebar is rendering');
-
   return (
-    
     <div className="sidebar">
-      <img src={HomeIcon} alt="Home" className="sidebar-icon" />
-      <img src={ProfileIcon} alt="Profile" className="sidebar-icon" />
-      <img src={NotificationIcon} alt="Notification" className="sidebar-icon" />
-      <img src={CalenderIcon} alt="Calender" className="sidebar-icon" />
+      <Link to="/">
+        <img src={HomeIcon} alt="Home" className="sidebar-icon" />
+      </Link>
+      <Link to="/profile">
+        <img src={ProfileIcon} alt="Profile" className="sidebar-icon" />
+      </Link>
+      <Link to="/calendar">
+        <img src={CalenderIcon} alt="Calendar" className="sidebar-icon" />
+      </Link>
     </div>
   );
 }
+
 export default Sidebar;
